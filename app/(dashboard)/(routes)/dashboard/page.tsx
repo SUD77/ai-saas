dc67d78e2c6745d3fe6cf64a0b9101bb01e90572
sudhanshujaiswal <jaiswalsudhanshu20@gmail.com>
2023-11-09 11:13:01 +0530

    Did folder setup for landing and dashboard page

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+
+export default function DashBoardPage() {
+  return (
+   <p>Dashboard Page (Protected)</p>
+  ) 
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Genius',
-  description: 'Ai Platform',
+  description: 'AI Platform',
 }
 
 export default function RootLayout({
