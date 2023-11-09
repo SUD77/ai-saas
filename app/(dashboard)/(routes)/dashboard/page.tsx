78521063c9f6f33d1b81619199dc2c77bd26bc34
sudhanshujaiswal <jaiswalsudhanshu20@gmail.com>
2023-11-10 04:07:43 +0530

    setup of clerk auth for login and register

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashBoardPage() {
   return (
+    <div>
    <p>Dashboard Page (Protected)</p>
+   <UserButton afterSignOutUrl="/" />               {/* Below button shows the currenly logged in user */}     
+   </div>
   ) 
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
   return (
     <div>
       Landing Page (Unprotected)
+      {/* Below ones, are login and register button, which redirect us to respective screens */}
+      <div>
+        <Link href="/sign-in"> 
+          <Button>
+            Login
+          </Button>
+        </Link>
+        <Link href="/sign-up"> 
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default LandingPage;
